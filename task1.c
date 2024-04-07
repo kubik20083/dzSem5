e0ac3fc39cb31ead243875023fb3cd5e57dda7a2
kubik20083 <kubik20083@mail.ru>
2024-04-07 20:29:06 +0300

    task1

--- task1.c
+++ task1.c
@@ -1,10 +1,18 @@
 // Задача 1. Сколько зерен на доске?
 
 #include<stdio.h>
+int grains (int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; i++)
+        result = result * 2;
+    return result;
+}
 
 int main(int argc, char const *argv[])
 {
-    for(int i = 1; i <= 64; i ++)
-    
+    int n;
+    scanf("%d", &n);
+    printf("Number of grains per cell: %d\n", grains(n));
     return 0;
 }
